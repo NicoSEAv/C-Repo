de313e88541ea43202cac30a603a6c54b25a64da
Nicolò Buonfrate <nicobuonfrate@Mac.Home>
2025-10-07 00:42:31 +0200

    First Programm

--- 03.FirstSteps/3.2FirstCppProgram/main.cpp
+++ 03.FirstSteps/3.2FirstCppProgram/main.cpp
@@ -2,9 +2,20 @@
 
 int main(){
 
-    std::cout << "Number1" << std::endl;
-    std::cout << "Number2" << std::endl;
-    std::cout << "Number3" << std::endl;
+    // std::cout << "Number1" << std::endl;
+    // std::cout << "Number2" << std::endl;
+    // std::cout << "Number3" << std::endl;
 
     return 0;
 }
+
+// #include <iostream>
+// #include <thread>
+// #include <chrono>
+
+// int main() {
+//     std::cout << "Sto lavorando..." << std::endl;
+//     std::this_thread::sleep_for(std::chrono::seconds(5));
+//     std::cout << "fatto!" << std::endl;
+//     return 0;
+// }
